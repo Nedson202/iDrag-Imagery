6fc5710f32145bfaa6d925326642d63c5b06fd58
Nedson202 <simeonsamson4@gmail..com>
2019-02-02 07:04:43 +0100

    Updates

---
+++ precache-manifest.676e2edac5de3913b7f5286da84f5db9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2174d17e94189fe8843f",
+    "url": "/iDrag-Imagery/static/js/runtime~main.2174d17e.js"
+  },
+  {
+    "revision": "451425cf7d003a91e8ce",
+    "url": "/iDrag-Imagery/static/js/main.451425cf.chunk.js"
+  },
+  {
+    "revision": "bcd853161c3323b35ab0",
+    "url": "/iDrag-Imagery/static/js/1.bcd85316.chunk.js"
+  },
+  {
+    "revision": "451425cf7d003a91e8ce",
+    "url": "/iDrag-Imagery/static/css/main.75b61198.chunk.css"
+  },
+  {
+    "revision": "1e53922d6bf8fe98cc50d8a80519c55d",
+    "url": "/iDrag-Imagery/index.html"
+  }
+];
